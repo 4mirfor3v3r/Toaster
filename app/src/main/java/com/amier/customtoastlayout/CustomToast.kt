606833ed2a4c1c3d6bc341e01d3c0ab96f43d0ff
ahmad_amirudin <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2020-03-04 09:20:12 +0700

    Initial Commit

--- app/src/main/java/com/amier/customtoastlayout/CustomToast.kt
+++ app/src/main/java/com/amier/customtoastlayout/CustomToast.kt
@@ -7,16 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 
-fun make(context: Context, idReslayout: Int, Toastduration: Int):Toast{
-    val v = idReslayout as View
-    val viewgroup = v.parent as ViewGroup
-    val layout = LayoutInflater.from(context).inflate(idReslayout, viewgroup)
+class CustomToast {
+    fun make(context: Context, idReslayout: Int, Toastduration: Int): Toast {
+        val v = idReslayout as View
+        val viewgroup = v.parent as ViewGroup
+        val layout = LayoutInflater.from(context).inflate(idReslayout, viewgroup)
 
-    val toast = Toast(context.applicationContext)
-    toast.apply {
-        setGravity(Gravity.BOTTOM,0,0)
-        view = layout
-        duration = Toastduration
+        val toast = Toast(context.applicationContext)
+        toast.apply {
+            setGravity(Gravity.BOTTOM, 0, 0)
+            view = layout
+            duration = Toastduration
+        }
+        return toast
     }
-    return toast
- }
+}
