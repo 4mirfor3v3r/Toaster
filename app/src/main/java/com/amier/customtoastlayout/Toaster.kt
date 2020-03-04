9055f97dd9be160c38766d1f03e8ea7afaa9a91f
ahmad_amirudin <ahmad_amirudin_27rpl@student.smktelkom-mlg.sch.id>
2020-03-04 13:42:16 +0700

    1.0.0.develop

--- app/src/main/java/com/amier/customtoastlayout/Toaster.kt
+++ app/src/main/java/com/amier/customtoastlayout/Toaster.kt
@@ -1,15 +1,12 @@
 package com.amier.customtoastlayout
 
 import android.content.Context
-import android.content.res.Resources
-import android.content.res.XmlResourceParser
 import android.view.*
 import android.widget.Toast
-import androidx.core.view.ViewGroupCompat
 
 class Toaster {
     companion object {
-        fun make(context: Context, idReslayout: Int, Toastduration: Int): Toast {
+        fun make(context: Context, idReslayout: Int, toastDuration: Int): Toast {
             val vs = context.resources.getLayout(idReslayout)
             val layout = LayoutInflater.from(context).inflate(vs,null,false)
 
@@ -17,12 +14,12 @@ class Toaster {
             toast.apply {
                 setGravity(Gravity.BOTTOM, 0, 0)
                 view = layout
-                duration = Toastduration
+                duration = toastDuration
                 show()
             }
             return toast
         }
-        fun make(context: Context, idReslayout: Int, gravity: Int, Toastduration: Int): Toast {
+        fun make(context: Context, idReslayout: Int, gravity: Int, toastDuration: Int): Toast {
             val vs = context.resources.getLayout(idReslayout)
             val layout = LayoutInflater.from(context).inflate(vs,null,false)
 
@@ -30,7 +27,7 @@ class Toaster {
             toast.apply {
                 setGravity(gravity, 0, 0)
                 view = layout
-                duration = Toastduration
+                duration = toastDuration
                 show()
             }
             return toast
